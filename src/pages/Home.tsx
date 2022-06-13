a9847414342e7c11c481de5f3188238ce812f0be
stanislaukostka <zelasikas@gmail.com>
2022-06-13 17:37:01 +0300

    fix layout, add buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
-import {IonButton, IonContent} from '@ionic/react';
-import './App.css';
-import {NavLink} from 'react-router-dom';
-// import { useEffect, useState } from 'react';
-// //REDUX
-// import { useSelector, useDispatch } from 'react-redux'
+import {IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import '../App.css';
+import {NavLink, useHistory} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+//REDUX
+import { useSelector, useDispatch } from 'react-redux'
 
 
 const Home: React.FC = () => {
     return (
         <IonContent className="startPage" fullscreen>
-            <NavLink to="trainer">
-                <IonButton className='button'>START</IonButton>
+            <NavLink to="exercise">
+                <IonButton className='menuButton'>START</IonButton>
             </NavLink>
         </IonContent>
     );
