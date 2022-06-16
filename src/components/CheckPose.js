443702ca46aabf363b2f3a17b1aa9e3f80c63f98
stanislaukostka <zelasikas@gmail.com>
2022-06-16 10:20:32 +0300

    new properties of plug

--- src/components/CheckPose.js
+++ src/components/CheckPose.js
@@ -1,7 +1,8 @@
+import { color } from "d3";
 import findAngle from "./findAngle";
+import checkBody from "./checkBody";
 
-function CheckExercise(poseLandmarks, exerciseName, find_angle) {
-
+function CheckExercise(poseLandmarks, exerciseValue) {
     const colors = {
         arm: {
             left: 'white',
@@ -19,126 +20,226 @@ function CheckExercise(poseLandmarks, exerciseName, find_angle) {
         }
     }
 
-    if(!poseLandmarks) return colors;
+    if (!poseLandmarks) return colors;
 
-    // const left_shoulder = [poseLandmarks[11]?.x, poseLandmarks[11]?.y, poseLandmarks[11]?.z]
-    // const right_shoulder = [poseLandmarks[12].x, poseLandmarks[12].y, poseLandmarks[12].z]
-    // const left_elbow = [poseLandmarks[13].x, poseLandmarks[13].y, poseLandmarks[13].z]
-    // const right_elbow = [poseLandmarks[14].x, poseLandmarks[14].y, poseLandmarks[14].z]
-    // const left_wrist = [poseLandmarks[15].x, poseLandmarks[15].y, poseLandmarks[15].z]
-    // const right_wrist = [poseLandmarks[16].x, poseLandmarks[16].y, poseLandmarks[16].z]
-    // const left_hip = [poseLandmarks[23].x, poseLandmarks[23].y, poseLandmarks[23].z]
-    // const right_hip = [poseLandmarks[24].x, poseLandmarks[24].y, poseLandmarks[24].z]
-    // const left_knee = [poseLandmarks[25].x, poseLandmarks[25].y, poseLandmarks[25].z]
-    // const right_knee = [poseLandmarks[26].x, poseLandmarks[26].y, poseLandmarks[26].z]
-    // const left_ankle = [poseLandmarks[27].x, poseLandmarks[27].y, poseLandmarks[27].z]
-    // const right_ankle = [poseLandmarks[28].x, poseLandmarks[28].y, poseLandmarks[28].z]
-    // const left_foot = [poseLandmarks[31].x, poseLandmarks[31].y, poseLandmarks[31].z]
-    // const right_foot = [poseLandmarks[32].x, poseLandmarks[32].y, poseLandmarks[32].z]
+    if (exerciseValue === 0) {
 
-    // Function to find the angle in 3D space
+        if(checkBody(poseLandmarks)){
+            goodMorning()
+        }
+    };
+
+    if (exerciseValue === 1) cabaret();
+    if (exerciseValue === 2) march_in_place();
+    if (exerciseValue === 3) leg_push();
+    if (exerciseValue === 4) high_knee_march();
+    if (exerciseValue === 5) squat();
 
 
-    if (exerciseName === 1) goodMorning();
-    if (exerciseName === 2) cabaret();
 
     function goodMorning() {
+        const angle_back_right = findAngle(12, 24, 26, poseLandmarks)
+        const angle_leg_right = findAngle(24, 26, 28, poseLandmarks)
 
-        const angle_elbow_left=findAngle(15, 13, 11, poseLandmarks)
-        const angle_elbow_right=findAngle(16, 14, 12, poseLandmarks)
 
-        const angle_back_right=findAngle(12, 24, 26, poseLandmarks)
+        let stage = "DOWN"
 
-        //const angle_leg_left=findAngle(23, 25, 27, poseLandmarks)
-        const angle_leg_right=findAngle(24, 26, 28, poseLandmarks)
-
-        let stage = "START"
-
-        if (angle_elbow_left>=40 && angle_elbow_right>=40 && angle_elbow_left<=80 && angle_elbow_right<=80){
-            colors.arm.left = "green"
-            colors.arm.right="green"
-            stage ="KNEE"
+        if (stage === "UP" && angle_leg_right >= 120 && angle_leg_right <= 160) {
+            colors.leg.left = "green"
+            colors.leg.right = "green"
+            stage = "DOWN"
         }
 
 
-        if (stage==="KNEE" && angle_leg_right>=120 && angle_leg_right<=160){
-            colors.leg.left="green"
-            colors.leg.right="green"
-            stage="DOWN"
-        }
-
-
-        if (stage==="DOWN" && angle_back_right>=90 && angle_back_right<=130){
-            colors.body.up="green"
-            colors.body.down="green"
-            stage="UP"
+        if (stage === "DOWN" && angle_back_right >= 90 && angle_back_right <= 130) {
+            colors.body.up = "green"
+            colors.body.down = "green"
+            stage = "UP"
         }
 
     }
 
-    function cabaret(){
+    function cabaret() {
 
         let stage = "START"
-        let side =""
+        let side = ""
         let counter = 0
 
-        const angle_shoulder_left=findAngle(13, 11, 23, poseLandmarks)
-        const angle_shoulder_right= findAngle(14, 12, 24, poseLandmarks)
+        const angle_shoulder_left = findAngle(13, 11, 23, poseLandmarks)
+        const angle_shoulder_right = findAngle(14, 12, 24, poseLandmarks)
 
-        const angle_knee_right=findAngle(24, 26, 28, poseLandmarks)
-        const angle_knee_left=findAngle(23, 25, 27, poseLandmarks)
+        const angle_knee_right = findAngle(24, 26, 28, poseLandmarks)
+        const angle_knee_left = findAngle(23, 25, 27, poseLandmarks)
 
-        const angle_leg_right=findAngle(12, 24, 28, poseLandmarks)
-        const angle_leg_left=findAngle(11, 23, 27, poseLandmarks)
+        const angle_leg_right = findAngle(12, 24, 28, poseLandmarks)
+        const angle_leg_left = findAngle(11, 23, 27, poseLandmarks)
 
-        if (counter % 2 == 0) {
-            side="LEFT"
-            colors.arm.left="yellow"
-            colors.leg.right="yellow"
-        }
-        else  {
-            side="RIGHT"
-            colors.arm.right="yellow"
-            colors.leg.left="yellow"
-        }
-
-        if (angle_shoulder_left>=80  && angle_shoulder_right>=80)
-        {
-            colors.arm.left="green"
-            colors.arm.right="green"
-            stage="KNEE"
+        if (counter % 2 === 0) {
+            side = "LEFT"
+            colors.arm.left = "yellow"
+            colors.leg.right = "yellow"
         }
         else {
-            colors.arm.left="red"
-            colors.arm.right="red"
+            side = "RIGHT"
+            colors.arm.right = "yellow"
+            colors.leg.left = "yellow"
         }
+
+
+        if (angle_shoulder_left >= 80 && angle_shoulder_right >= 80) {
+            colors.arm.left = "green"
+            colors.arm.right = "green"
+            stage = "KNEE"
+        }
+        else {
+            colors.arm.left = "red"
+            colors.arm.right = "red"
+        }
+
 
         if (side === "LEFT") {
             if (stage === "KNEE" && angle_knee_right >= 80 && angle_knee_right <= 100) {
                 stage = "LEG"
-                colors.leg.right="green"
+                colors.leg.right = "green"
             }
             if (stage === "LEG" && angle_leg_right >= 90 && angle_leg_right <= 110) {
                 stage = "STAND"
                 counter += 1
-                colors.leg.right="green"
+                colors.leg.right = "green"
             }
         }
+
 
         if (side === "RIGHT") {
             if (stage === "KNEE" && angle_knee_left >= 80 && angle_knee_left <= 100) {
                 stage = "LEG";
-                colors.leg.left="green"
+                colors.leg.left = "green"
             }
 
             if (stage === "LEG" && angle_leg_left >= 90 && angle_leg_left <= 110) {
                 stage = "STAND";
                 counter += 1;
-                colors.leg.left="green"
+                colors.leg.left = "green"
             }
         }
 
     }
+
+    function march_in_place() {
+
+        let stage = "START"
+        let side = ""
+        let counter = 0
+
+        const angle_arm_left = findAngle(11, 13, 15, poseLandmarks)
+        const angle_arm_right = findAngle(12, 14, 16, poseLandmarks)
+
+        const angle_leg_left = findAngle(23, 25, 27, poseLandmarks)
+        const angle_leg_right = findAngle(24, 26, 28, poseLandmarks)
+
+        if (counter % 2 == 0) {
+            side = "LEFT"
+            stage = "UP"
+            colors.arm.left = "yellow"
+            colors.leg.right = "yellow"
+        }
+        else {
+            side = "RIGHT"
+            stage = "UP"
+            colors.arm.right = "yellow"
+            colors.leg.left = "yellow"
+        }
+
+
+        if (side === "LEFT" && angle_arm_left >= 20 && angle_arm_left <= 80 && angle_leg_right >= 70 && angle_leg_right <= 100) {
+            colors.arm.left = "green"
+            colors.leg.right = "green"
+            stage = "DOWN";
+            counter += 1;
+        }
+
+
+        if (side === "RIGHT" && angle_arm_right >= 20 && angle_arm_right <= 80 && angle_leg_left >= 70 && angle_leg_left <= 100) {
+            colors.arm.right = "green"
+            colors.leg.left = "green"
+            stage = "DOWN";
+            counter += 1;
+        }
+
+    }
+
+    function leg_push(){
+        let stage = "START"
+        let side = ""
+        let counter = 0
+
+        const angle_body_left = findAngle(13, 11, 23, poseLandmarks)
+        const angle_body_right = findAngle(14, 12, 24, poseLandmarks)
+
+        const angle_back = findAngle(12, 24, 26, poseLandmarks)
+
+        const angle_leg_r = findAngle(12, 24, 28, poseLandmarks)
+        const angle_leg_l = findAngle(11, 23, 27, poseLandmarks)
+
+        if (angle_body_right >= 160 && angle_body_left >= 160) {
+            colors.arm.left = "green"
+            colors.arm.right = "green"
+            stage = "DOWN";
+        }
+
+        if (stage === "DOWN" && angle_back >= 130 && angle_back <= 150) {
+            stage = "PUSH"
+        }
+
+        if (counter % 2 === 0) {
+            side = "LEFT"
+            colors.arm.right="yellow"
+            colors.leg.left="yellow"
+        }
+
+        else {
+            side = "RIGHT"
+            colors.arm.left="yellow"
+            colors.leg.right="yellow"
+        }
+
+        if (side === "LEFT" && stage === "PUSH" && angle_leg_l >= 110 && angle_leg_l <= 140) {
+            stage = "UP";
+            counter += 1;
+        }
+
+        if (side === "RIGHT" && stage === "PUSH" && angle_leg_r >= 110 && angle_leg_r <= 140) {
+            stage = "UP";
+            counter += 1;
+        }
+
+    }
+
+    function high_knee_march(){
+
+    }
+
+    function squat(){
+        let counter = 0
+        let stage =null;
+        const angle_leg_right = findAngle(24, 26, 28, poseLandmarks)
+        const angle_leg_left = findAngle(23, 25, 27, poseLandmarks)
+
+        colors.leg.right="yellow"
+        colors.leg.left="yellow"
+        if (angle_leg_left > 150 && angle_leg_right > 150) {
+            stage = "DOWN";
+        }
+
+        if (stage === "DOWN" && angle_leg_left <= 110 && angle_leg_left >= 90 && angle_leg_right <= 110 && angle_leg_right >= 90) {
+            stage = "UP";
+            colors.leg.right="green"
+            colors.leg.left="green"
+            counter += 1;
+        }
+
+    }
+
 
     return {
         colors: colors,
--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -1,4 +1,5 @@
 import { FaceMesh } from "@mediapipe/face_mesh";
+import { IonButton, IonCheckbox, IonItem, IonLabel, IonList } from "@ionic/react";
 import React, { useRef, useEffect, useState } from "react";
 import { Pose } from "@mediapipe/pose";
 import { Hands } from "@mediapipe/hands";
@@ -12,12 +13,24 @@ import CheckPose from "./CheckPose";
 import findAngle from "./findAngle";
 import Interface from "./interface";
 
-export default function Trainer({exerciseName}) {
+import checkBody from "./checkBody";
+
+
+export default function Trainer({exerciseValue, exerciseName}) {
+    const rightDots = [11,12,13,14,15,16,23,24,25,26,27,28]
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
     const [leftHandAngle, setLeftHandAngle] = useState(0)
+    const [crazyRule,setCrazyRule] = useState([])
+    const [precent, setPrecent] = useState(null)
+    const [rules,setRules] = useState([])
     const [leftHandColor,setLeftHandColor] = useState('#fff')
     const [dots,setDots] = useState(undefined)
+    const [dotsForAngle,setDotsForAngle] = useState([])
+    const [angle,setAngle] = useState(null)
+    const [visibleBody, setVisibleBody] = useState(false)
+    const [checking,setChecking]=useState(false)
+    const [delay,setDelay] = useState(0)
     const [colors,setColors] = useState({
         arm: {
             left: 'white',
@@ -34,13 +47,24 @@ export default function Trainer({exerciseName}) {
             right: 'white'
         }
     })
+
+
     var camera = null;
 
     function onResults(results) {
+        //Видимость полной позы человека
+
+        if(results.poseLandmarks){
+
+        }
+
+
         // const video = webcamRef.current.video;
+
         const videoWidth = webcamRef.current.video.videoWidth;
         const videoHeight = webcamRef.current.video.videoHeight;
         // Set canvas width
+
         canvasRef.current.width = videoWidth;
         canvasRef.current.height = videoHeight;
         const canvasElement = canvasRef.current;
@@ -55,10 +79,11 @@ export default function Trainer({exerciseName}) {
             canvasElement.height
         );
 
-        const poseInfo = CheckPose(results.poseLandmarks, exerciseName)
+        const poseInfo = CheckPose(results.poseLandmarks, exerciseValue)
 
         if(results.poseLandmarks) {
             setLeftHandAngle(findAngle(16,14,11,results.poseLandmarks))
+            setVisibleBody(checkBody(results.poseLandmarks))
             setLeftHandColor(`${poseInfo.colors.arm.right}`)
             setDots(results.poseLandmarks)
             setColors(poseInfo.colors)
@@ -72,8 +97,9 @@ export default function Trainer({exerciseName}) {
                 return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
             }});
 
+
         pose.setOptions({
-            modelComplexity: 0.5,
+            modelComplexity: 1,
             smoothLandmarks: true,
             enableSegmentation: true,
             smoothSegmentation: false,
@@ -98,27 +124,172 @@ export default function Trainer({exerciseName}) {
         }
     }, []);
 
+    useEffect(()=>{
+        if(dotsForAngle.length === 3) {
+            setAngle(findAngle(dotsForAngle[0],dotsForAngle[1],dotsForAngle[2], dots))
+            let allRules = rules.slice()
+            allRules.push({dots:dotsForAngle, angle: findAngle(dotsForAngle[0],dotsForAngle[1],dotsForAngle[2], dots)})
+            setRules(allRules)
+            setDotsForAngle([])
+        }
+    },[dotsForAngle])
+
+    useEffect(()=>{
+        console.log(rules)
+    },[rules])
+
+    function selectDot(dotNumber){
+
+        if(dotsForAngle.length < 3){
+            let a = dotsForAngle.slice()
+            a.push(dotNumber)
+            setDotsForAngle(a)
+        }
+    }
+
+    function createRules(){
+        var dotsSet = (function(arr, limit){
+            var results = [], result, mask, total = Math.pow(2, arr.length);
+            for(mask = 0; mask < total; mask++){
+                result = [];
+                let i = arr.length - 1;
+                do{
+                    if( (mask & (1 << i)) !== 0){
+                        result.push(arr[i]);
+                    }
+                }while(i--);
+                if( result.length == limit){
+                    results.push(result);
+                }
+            }
+            return results;
+        })(rightDots, 3);
+
+        let angles = []
+        dotsSet.forEach(element => {
+            angles.push(findAngle(element[0],element[1],element[2],dots))
+        });
+
+        setCrazyRule(angles)
+        console.log(angles)
+    }
+
+    function checkRules(){
+        var dotsSet = (function(arr, limit){
+            var results = [], result, mask, total = Math.pow(2, arr.length);
+            for(mask = 0; mask < total; mask++){
+                result = [];
+                let i = arr.length - 1;
+                do{
+                    if( (mask & (1 << i)) !== 0){
+                        result.push(arr[i]);
+                    }
+                }while(i--);
+                if( result.length == limit){
+                    results.push(result);
+                }
+            }
+            return results;
+        })(rightDots, 3);
+
+        let trueAngles = 0
+        let falseAngles = 0
+
+        let angles = []
+
+        dotsSet.forEach(element => {
+            angles.push(findAngle(element[0],element[1],element[2],dots))
+        });
+
+        angles.forEach((el,ind)=>{
+            if(((el < crazyRule[ind] + 5) && (el > crazyRule[ind] - 5))){
+                trueAngles = trueAngles + 1
+            }else {
+                falseAngles = falseAngles + 1
+            }
+        })
+        setPrecent(Math.floor((trueAngles/220)*100))
+        // console.log(trueAngles,falseAngles, `${Math.floor((trueAngles/220)*100)}%`)
+    }
+
+    useEffect(()=>{
+        if(checking){
+            let timer = setTimeout(()=>{
+                checkRules()
+                return () =>{
+                    clearTimeout(timer)
+                }
+            },10)
+        }
+    },[dots, checking])
+
     return (
-        <center>
-            <div className="drawBox">
-                <Webcam
-                    width={"1280"}
-                    height={"720"}
-                    ref={webcamRef}
-                />
-                <canvas ref={canvasRef} className="draw"/>
-            </div>
-            <div className="settings">
-                <div className="angles">
-                    <p>Right hand angle:
-                        <span style={{color: leftHandColor}}>{Math.round(leftHandAngle*10)/10}°</span>
-                    </p>
+        <div className="exerciseView">
+            <div className="poseView">
+                <div className="drawBox">
+                    <Webcam
+                        width={"1280"}
+                        height={"720"}
+                        ref={webcamRef}
+                    />
+                    <canvas ref={canvasRef} className="draw"/>
+                    {visibleBody &&
+                        <Interface dots={dots} colors={colors}/>
+                    }
                 </div>
             </div>
-            <div className="svgBox">
-                <Interface dots = {dots} colors = {colors}/>
+            <div className="exerciseStateView">
+                <div className="textBox">
+                    <h1>Excersice name: {exerciseName}</h1>
+                </div>
+                <div className="dotBox">
+                    {
+                        rightDots.map((value,index)=>{
+                            return <IonButton className="dotNumber" key={index} expand="block" color="primary" onClick={()=>{
+                                if(dotsForAngle.length > 0){
+                                    if(value !== dotsForAngle[0] &&
+                                        value !== dotsForAngle[1] &&
+                                        value !== dotsForAngle[2]){
+                                        selectDot(value)
+                                    }
+                                }else {
+                                    selectDot(value)
+                                }
+
+                            }}>{value}</IonButton>
+                        })
+                    }
+                </div>
+                <div className="selectedDotsForAngle">
+                    {
+                        dotsForAngle.map((item,index)=>{
+                            return <IonButton className="selectedDotNumber" key={index} expand="block" color="success">{item}</IonButton>
+                        })
+                    }
+                </div>
+
+                {angle && <div className="angleBox">
+                    <h1 >{Math.floor(angle*10)/10}°</h1>
+                </div>
+                }
+                <IonButton className="createRules" expand="block" color="success" onClick={()=>{createRules()}}>CREATE RULES</IonButton>
+                {(crazyRule.length > 0) && <IonButton className="checkRule" expand="block" color="success" onClick={()=>{setChecking(!checking)}}>{checking? "UNCHECK" : "CHECK"} RULES</IonButton>}
+                {precent && <div className="precentBox">
+                    <h1 style={{color: `rgba(${200 - precent*2},${precent * 2},0,0.9)`}}>{precent}%</h1>
+                </div>}
             </div>
-        </center>
+            {(!visibleBody || !dots) &&
+                <div className="foregroundView">
+                    <div className="foregroundTextBox">
+                        {!dots
+                            ?<h1 className="foregroundText">Connecting to camera......</h1>
+                            :<h1 className="foregroundText">Stand up to your full height</h1>
+                        }
+                    </div>
+                </div>
+            }
+
+        </div>
     );
 }
 
